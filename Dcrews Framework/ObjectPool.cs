c0c314b889d409982b495e4842547595f94ab71c
Dcrew <dean.reynolds@live.co.uk>
2019-06-07 18:33:18 +0100

    update object pool

--- Dcrews Framework/ObjectPool.cs
+++ Dcrews Framework/ObjectPool.cs
@@ -12,45 +12,38 @@ namespace Dcrew.Framework
         public delegate T NewObjEvent();
         public delegate void ToggleEvent(T obj);
 
-        readonly Queue<T> _inactive;
-        readonly HashSet<T> _active;
+        readonly Stack<T> _inactive;
         readonly NewObjEvent _newObj;
 
         public ObjectPool(NewObjEvent newObj)
         {
             _newObj = newObj;
-            _inactive = new Queue<T>();
-            _active = new HashSet<T>();
+            _inactive = new Stack<T>();
         }
         public ObjectPool(NewObjEvent newObj, int initialCapacity)
         {
             _newObj = newObj;
-            _inactive = new Queue<T>(initialCapacity);
-            _active = new HashSet<T>(initialCapacity);
+            _inactive = new Stack<T>(initialCapacity);
             Expand(initialCapacity);
         }
 
         public void Expand(int amount)
         {
             for (int i = 0; i < amount; i++)
-                _inactive.Enqueue(_newObj());
+                _inactive.Push(_newObj());
         }
 
         public T Activate()
         {
             if (_inactive.Count <= 0)
                 Expand(_overAllocateAmount);
-            var obj = _inactive.Dequeue();
-            _active.Add(obj);
+            var obj = _inactive.Pop();
             WakeObj?.Invoke(obj);
             return obj;
         }
         public bool Deactivate(T obj)
         {
-            if (!_active.Contains(obj))
-                return false;
-            _active.Remove(obj);
-            _inactive.Enqueue(obj);
+            _inactive.Push(obj);
             ResetObj?.Invoke(obj);
             return true;
         }
