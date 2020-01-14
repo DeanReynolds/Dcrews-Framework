cf94fc40c7f044e22237f5f242916312f32fab2e
Dcrew <dean.reynolds@live.co.uk>
2020-01-14 22:53:47 +0000

    fix, generalize & docu spatial hash

--- Dcrews Framework/SpatialHash.cs
+++ Dcrews Framework/SpatialHash.cs
@@ -3,60 +3,63 @@ using System.Collections.Generic;
 
 namespace Dcrew.Framework
 {
-    public class SpatialHash<T> where T : IAABB
+    /// <summary>For very fast but approximate spatial partitioning</summary>
+    public static class SpatialHash<T> where T : IAABB
     {
-        readonly Dictionary<int, HashSet<T>> _hash = new Dictionary<int, HashSet<T>>();
-        readonly int _spacing;
-        readonly Dictionary<T, int> _objsAdded = new Dictionary<T, int>();
+        const int DEFAULT_SPACING = 50;
 
-        public SpatialHash(int spacing) { _spacing = spacing; }
-
-        public void Add(T obj)
+        /// <summary>Set to your largest object collision radius. Defaults at 50</summary>
+        public static int Spacing
         {
-            var bucket = (17 * 23 + (obj.AABB.Center.X / _spacing).GetHashCode()) * 23 + (obj.AABB.Center.Y / _spacing).GetHashCode();
-            _objsAdded.Add(obj, bucket);
-            if (!_hash.ContainsKey(bucket))
+            get => _spacing;
+            set
             {
-                var set = Pool<HashSet<T>>.Spawn();
-                set.Clear();
-                set.Add(obj);
-                _hash.Add(bucket, set);
-                return;
+                _spacing = value;
+                foreach (var obj in _objsAdded.Keys)
+                    Update(obj);
             }
-            _hash[bucket].Add(obj);
         }
-        public bool Update(T obj)
+
+        static readonly HashSet<HashSet<T>> _setsToRecycle = new HashSet<HashSet<T>>();
+        static readonly IDictionary<int, HashSet<T>> _hash = new Dictionary<int, HashSet<T>>();
+        static readonly IDictionary<T, int> _objsAdded = new Dictionary<T, int>();
+
+        static int _spacing = DEFAULT_SPACING;
+
+        public static void Add(T obj)
         {
-            if (!_objsAdded.ContainsKey(obj))
-                return false;
-            var bucket = (17 * 23 + (obj.AABB.Center.X / _spacing).GetHashCode()) * 23 + (obj.AABB.Center.Y / _spacing).GetHashCode();
+            var bucket = Bucket(obj);
+            Add(obj, bucket);
+            _objsAdded.Add(obj, bucket);
+        }
+        public static bool Update(T obj)
+        {
+            var bucket = Bucket(obj);
             if (bucket == _objsAdded[obj])
                 return false;
             _hash[_objsAdded[obj]].Remove(obj);
+            Add(obj, bucket);
             _objsAdded[obj] = bucket;
-            if (!_hash.ContainsKey(bucket))
-            {
-                var set = Pool<HashSet<T>>.Spawn();
-                set.Clear();
-                set.Add(obj);
-                _hash.Add(bucket, set);
-                return true;
-            }
-            _hash[bucket].Add(obj);
             return true;
         }
-        public void Remove(T obj)
+        public static void Remove(T obj)
         {
             _hash[_objsAdded[obj]].Remove(obj);
             _objsAdded.Remove(obj);
         }
-
-        public bool Contains(T obj) => _objsAdded.ContainsKey(obj);
-
-        public HashSet<T> Query(Vector2 position)
+        public static void Clear()
         {
-            var xn1 = (int)(position.X / _spacing);
-            var yn1 = (int)(position.Y / _spacing);
+            _hash.Clear();
+            _objsAdded.Clear();
+        }
+
+        public static bool Contains(T obj) => _objsAdded.ContainsKey(obj);
+
+        /// <summary>Call <see cref="Recycle(HashSet{T})"/> on the returned set when done with it</summary>
+        public static HashSet<T> Query(Vector2 position)
+        {
+            var xn1 = (int)(position.X / Spacing);
+            var yn1 = (int)(position.Y / Spacing);
             var bucket = (17 * 23 + xn1.GetHashCode()) * 23 + yn1.GetHashCode();
             var objs = Pool<HashSet<T>>.Spawn();
             objs.Clear();
@@ -86,18 +89,36 @@ namespace Dcrew.Framework
             bucket = (17 * 23 + (xn1 + 1).GetHashCode()) * 23 + (yn1 + 1).GetHashCode();
             if (_hash.TryGetValue(bucket, out set))
                 objs.UnionWith(set);
+            _setsToRecycle.Add(objs);
             return objs;
         }
 
-        public void Clear()
-        {
-            _hash.Clear();
-            _objsAdded.Clear();
-        }
-
-        public void Recycle(HashSet<T> objs)
+        public static void Recycle(HashSet<T> objs)
         {
             Pool<HashSet<T>>.Free(objs);
+            _setsToRecycle.Remove(objs);
+        }
+        public static void Recycle()
+        {
+            foreach (var set in _setsToRecycle)
+                Pool<HashSet<T>>.Free(set);
+            _setsToRecycle.Clear();
+        }
+
+        static int Bucket(Point p) => (17 * 23 + (p.X / Spacing).GetHashCode()) * 23 + (p.Y / Spacing).GetHashCode();
+        static int Bucket(T obj) => Bucket(obj.AABB.Center);
+
+        static void Add(T obj, int bucket)
+        {
+            if (!_hash.ContainsKey(bucket))
+            {
+                var set = Pool<HashSet<T>>.Spawn();
+                set.Clear();
+                set.Add(obj);
+                _hash.Add(bucket, set);
+                return;
+            }
+            _hash[bucket].Add(obj);
         }
     }
 }
