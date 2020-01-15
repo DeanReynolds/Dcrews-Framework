7d2596441962c99cac0a6115ddcfd9075bde7d80
Dcrew <dean.reynolds@live.co.uk>
2020-01-15 23:27:24 +0000

    fix & clean quad tree

--- Dcrews Framework/QuadTree.cs
+++ Dcrews Framework/QuadTree.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Dcrew.Framework
 {
-    /// <summary>For fast and accurate spatial partitioning</summary>
+    /// <summary>For fast and accurate spatial partitioning. Set <see cref="Bounds"/> before use</summary>
     public static class QuadTree<T> where T : IAABB
     {
         public static Rectangle Bounds
@@ -14,17 +14,20 @@ namespace Dcrew.Framework
             {
                 if (_mainNode != null)
                 {
-                    var items = _mainNode.AllItems;
+                    var items = _stored.Keys.ToArray();
+                    Pool<Node>.Free(_mainNode);
                     _mainNode = Pool<Node>.Spawn();
                     _mainNode.Bounds = value;
                     foreach (var i in items)
-                        _mainNode.Add(i);
+                        _stored[i] = _mainNode.Add(i);
                 }
                 else
                 {
                     _mainNode = Pool<Node>.Spawn();
                     _mainNode.Bounds = value;
                 }
+                _mainNode._parent = null;
+                Pool<Node>.EnsureSize(MGMath.Ceil(_mainNode.Bounds.Width / 32f * _mainNode.Bounds.Height / 32f));
             }
         }
 
@@ -36,53 +39,43 @@ namespace Dcrew.Framework
 
         public static bool Add(T item)
         {
-            if (_stored.ContainsKey(item))
-                return false;
-            Node node;
-            if ((node = _mainNode.Add(item)) != null)
+            Node n;
+            bool t = _stored.ContainsKey(item);
+            if ((n = _mainNode.Add(item)) != null)
             {
-                _stored.Add(item, node);
+                _stored.Add(item, n);
                 return true;
             }
             return false;
         }
-        public static bool Remove(T item)
+        public static void Remove(T item)
         {
-            if (!_stored.ContainsKey(item))
-                return false;
-            if (_stored[item].Remove(item))
-            {
-                _stored.Remove(item);
-                return true;
-            }
-            return false;
+            _stored[item].Remove(item);
+            _stored.Remove(item);
         }
-        public static bool Update(T item)
+        public static void Update(T item)
         {
-            if (!_stored.ContainsKey(item))
-                return false;
-            if (_stored[item].Remove(item))
-            {
-                _stored.Remove(item);
-                Add(item);
-                return true;
-            }
-            return false;
+            Remove(item);
+            Add(item);
         }
 
         /// <summary>Call <see cref="Recycle(HashSet{T})"/> on the returned set when done with it</summary>
         public static HashSet<T> Query(Rectangle area)
         {
-            var result = _mainNode.Query(area);
-            _setsToRecycle.Add(result);
-            return result;
+            var r = Pool<HashSet<T>>.Spawn();
+            r.Clear();
+            r.UnionWith(_mainNode.Query(area));
+            _setsToRecycle.Add(r);
+            return r;
         }
         /// <summary>Call <see cref="Recycle(HashSet{T})"/> on the returned set when done with it</summary>
         public static HashSet<T> Query(Vector2 pos)
         {
-            var result = _mainNode.Query(new Rectangle(MGMath.Round(pos.X), MGMath.Round(pos.Y), 1, 1));
-            _setsToRecycle.Add(result);
-            return result;
+            var r = Pool<HashSet<T>>.Spawn();
+            r.Clear();
+            r.UnionWith(_mainNode.Query(new Rectangle(MGMath.Round(pos.X), MGMath.Round(pos.Y), 1, 1)));
+            _setsToRecycle.Add(r);
+            return r;
         }
 
         public static void Recycle(HashSet<T> objs)
@@ -99,106 +92,102 @@ namespace Dcrew.Framework
 
         public class Node : IPoolable
         {
+            const int MAX_ITEMS = 16;
+
             public Rectangle Bounds { get; internal set; }
 
-            public bool IsEmpty => (_items.Count == 0) && (_nodes.Count == 0);
+            public bool IsEmpty => _items.Count == 0 && _nodes.Count == 0;
             public int AllCount
             {
                 get
                 {
-                    int count = 0;
-                    count += _items.Count;
-                    foreach (var node in _nodes)
-                        count += node.AllCount;
-                    return count;
+                    int c = _items.Count;
+                    foreach (var n in _nodes)
+                        c += n.AllCount;
+                    return c;
                 }
             }
             public HashSet<T> AllItems
             {
                 get
                 {
-                    var results = Pool<HashSet<T>>.Spawn();
-                    results.Clear();
-                    results.UnionWith(_items);
-                    foreach (var node in _nodes)
-                        results.UnionWith(node.AllItems);
-                    return results;
+                    _items2.Clear();
+                    _items2.UnionWith(_items);
+                    foreach (var n in _nodes)
+                        _items2.UnionWith(n.AllItems);
+                    return _items2;
                 }
             }
 
-            readonly HashSet<T> _items = Pool<HashSet<T>>.Spawn();
-            readonly List<Node> _nodes = Pool<List<Node>>.Spawn();
+            readonly HashSet<T> _items = new HashSet<T>(),
+                _items2 = new HashSet<T>();
+            readonly HashSet<Node> _nodes = new HashSet<Node>();
+
+            internal Node _parent;
+
 
             public Node Add(T item)
             {
-                if (_items.Count >= 16 && _nodes.Count == 0 && Bounds.Width * Bounds.Height > 1024)
+                Node n2;
+                if (_items.Count >= MAX_ITEMS && _nodes.Count == 0 && Bounds.Width * Bounds.Height > 1024)
                 {
                     CreateSubNodes();
-                    var itemsToRemove = Pool<HashSet<T>>.Spawn();
+                    _items2.Clear();
                     foreach (var i in _items)
-                    {
-                        var addedToANode = false;
-                        foreach (var node in _nodes)
-                            if (node.Bounds.Contains(i.AABB.Center))
+                        foreach (var n in _nodes)
+                            if (n.Bounds.Contains(i.AABB.Center))
                             {
-                                Node node2;
-                                if ((node2 = node.Add(i)) != null)
+                                if ((n2 = n.Add(i)) != null)
                                 {
-                                    addedToANode = true;
-                                    _stored[i] = node2;
+                                    _items2.Add(i);
+                                    _stored[i] = n2;
                                 }
                                 break;
                             }
-                        if (addedToANode)
-                            itemsToRemove.Add(i);
-                    }
-                    foreach (var i in itemsToRemove)
-                        _items.Remove(i);
+                    _items.ExceptWith(_items2);
                 }
-                foreach (var node in _nodes)
-                    if (node.Bounds.Contains(item.AABB.Center))
-                    {
-                        Node node2;
-                        if ((node2 = node.Add(item)) != null)
-                            return node2;
-                    }
+                foreach (var n in _nodes)
+                    if (n.Bounds.Contains(item.AABB.Center))
+                        if ((n2 = n.Add(item)) != null)
+                            return n2;
                 _items.Add(item);
                 return this;
             }
-            public bool Remove(T item)
+            public void Remove(T item)
             {
-                if (_items.Contains(item))
+                _items.Remove(item);
+                if (_parent != null && _parent.AllCount < MAX_ITEMS)
                 {
-                    _items.Remove(item);
-                    if (AllCount == 0)
-                        _nodes.Clear();
-                    return true;
+                    var items = _parent.AllItems;
+                    foreach (var i in items)
+                    {
+                        _parent._items.Add(i);
+                        _stored[i] = _parent;
+                    }
+                    foreach (var n in _parent._nodes)
+                        Pool<Node>.Free(n);
+                    _parent._nodes.Clear();
                 }
-                return false;
             }
             public HashSet<T> Query(Rectangle area)
             {
-                var items = Pool<HashSet<T>>.Spawn();
-                items.Clear();
-                foreach (T item in _items)
-                    if (area.Intersects(item.AABB))
-                        items.Add(item);
-                foreach (var node in _nodes)
+                _items2.Clear();
+                foreach (T i in _items)
+                    if (area.Intersects(i.AABB))
+                        _items2.Add(i);
+                foreach (var n in _nodes)
                 {
-                    if (node.Bounds.Contains(area))
+                    if (n.Bounds.Contains(area))
                     {
-                        foreach (T item in node.Query(area))
-                            items.Add(item);
+                        _items2.UnionWith(n.Query(area));
                         break;
                     }
-                    if (area.Contains(node.Bounds))
-                        foreach (T item in node.AllItems)
-                            items.Add(item);
-                    else if (node.Bounds.Intersects(area))
-                        foreach (T item in node.Query(area))
-                            items.Add(item);
+                    if (area.Contains(n.Bounds))
+                        _items2.UnionWith(n.AllItems);
+                    else if (n.Bounds.Intersects(area))
+                        _items2.UnionWith(n.Query(area));
                 }
-                return items;
+                return _items2;
             }
 
             void CreateSubNodes()
@@ -209,19 +198,23 @@ namespace Dcrew.Framework
                     halfHeight = Bounds.Height / 2;
                 var topLeft = Pool<Node>.Spawn();
                 topLeft.Bounds = new Rectangle(Bounds.Left, Bounds.Top, halfWidth, halfHeight);
+                topLeft._parent = this;
                 _nodes.Add(topLeft);
                 var bottomLeft = Pool<Node>.Spawn();
                 int midY = Bounds.Top + halfHeight,
                     height = Bounds.Bottom - midY;
                 bottomLeft.Bounds = new Rectangle(Bounds.Left, midY, halfWidth, height);
+                bottomLeft._parent = this;
                 _nodes.Add(bottomLeft);
                 var topRight = Pool<Node>.Spawn();
                 int midX = Bounds.Left + halfWidth,
                     width = Bounds.Right - midX;
                 topRight.Bounds = new Rectangle(midX, Bounds.Top, width, halfHeight);
+                topRight._parent = this;
                 _nodes.Add(topRight);
                 var bottomRight = Pool<Node>.Spawn();
                 bottomRight.Bounds = new Rectangle(midX, midY, width, height);
+                bottomRight._parent = this;
                 _nodes.Add(bottomRight);
             }
 
